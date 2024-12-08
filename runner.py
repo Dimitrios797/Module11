dcb4593660acb13f85509042308ab3eafcb616bb
Dimitrios797 <146129395+Dimitrios797@users.noreply.github.com>
2024-12-08 18:40:19 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,3 +1,4 @@
+# runner.py
 class Runner:
     def __init__(self, name, speed):
         if not isinstance(name, str):
@@ -6,3 +7,29 @@ class Runner:
             raise ValueError(f"Скорость не может быть отрицательной, сейчас {speed}")
         self.name = name
         self.speed = speed
+        self.distance = 0
+
+    def run(self):
+        self.distance += self.speed
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
